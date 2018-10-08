51ec33a5666ac84e52586e82c1556f8e1204c53c
Jason Snider <root@jasonsnider.com>
2018-10-08 12:20:05 -0500

    Delete a user record

--- routes/api/users.js
+++ routes/api/users.js
@@ -87,5 +87,21 @@ router.put('/', function(req, res){
 
 });
 
+router.delete('/:userId', function(req,res){
+
+  var userId = req.params.userId;
+
+  Users.remove({'_id':userId}, function(err,removed){
+
+    if(err){
+      return res.json({success: false, error: err});
+    }
+
+    return res.json({success: true, status: removed});
+
+  });
+
+});
+
 
 module.exports = router;
