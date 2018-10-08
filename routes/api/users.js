6415441f5caf337a19be63cabb0b892f4a58ee5d
Jason Snider <root@jasonsnider.com>
2018-10-08 10:52:52 -0500

    Retrieve a single user by _id

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,4 +11,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:userId', function(req,res){
+  
+  var userId = req.params.userId;
+   Users.findOne({'_id':userId}, function(err, user){
+     if(err){
+      return res.json({'success':false, 'error': err});
+    }
+     return res.json({'success':true, 'user': user});
+   });
+ });
+
+
 module.exports = router;
