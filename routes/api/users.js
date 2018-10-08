65061eb74a8708571db616917f0ae88998da1090
Jason Snider <root@jasonsnider.com>
2018-10-08 11:27:42 -0500

    Create a new user record

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,6 +28,22 @@ router.get('/:userId', function(req,res){
 
 });
 
+router.post('/', function(req, res) {
+  Users.create(new Users({
+    username: req.body.username,
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }), function(err, user){
+    
+    if(err){
+      return res.json({success: false, user: req.body, error: err});
+    }
+
+    return res.json({success: true, user: user});
+    
+  });
+});
 
 
 module.exports = router;
