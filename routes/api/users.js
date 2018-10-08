4fb5ea5797ff22e4207b1584dfc068f25e02d515
Jason Snider <root@jasonsnider.com>
2018-10-08 11:23:54 -0500

    Whitespace fixes

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,24 +3,31 @@ var router = express.Router();
 var Users = require('../../models/users');
 
 router.get('/', function(req, res, next) {
+
   Users.find({},function(err, users){
     if(err){
      return res.json({'success':false, 'error': err});
-   }
+    }
+
     return res.json({'success':true, 'users': users});
   });
+
 });
 
 router.get('/:userId', function(req,res){
   
   var userId = req.params.userId;
-   Users.findOne({'_id':userId}, function(err, user){
-     if(err){
+
+  Users.findOne({'_id':userId}, function(err, user){
+    if(err){
       return res.json({'success':false, 'error': err});
     }
-     return res.json({'success':true, 'user': user});
-   });
- });
+
+    return res.json({'success':true, 'user': user});
+  });
+
+});
+
 
 
 module.exports = router;
