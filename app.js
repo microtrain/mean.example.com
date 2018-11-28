73c9706453a0b5df15c34c2f7dfa174b09d3686f
Jason Snider <jason@jasonsnider.com>
2018-11-28 10:47:18 -0600

    Add a non-API end point for accessing the authentication app

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var Users = require('./models/users');
 
+var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiAuthRouter = require('./routes/api/auth');
@@ -70,6 +71,7 @@ passport.deserializeUser(function(user, done){
 });
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/api/auth', apiAuthRouter);
 app.use('/api/users', apiUsersRouter);
---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('auth/index', { title: 'User Authentication' });
+});
+
+module.exports = router;
