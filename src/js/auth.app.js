c9ce0a1d30091ccf04ed4f5f25c7b0a6e8aa3856
Jason Snider <jason@jasonsnider.com>
2018-11-30 13:48:13 -0600

    Add a registration form

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -26,6 +26,56 @@ var authApp = (function() {
     app.innerHTML=form;
   }
 
+  function registrationForm(){
+    var app = document.getElementById('app');
+
+    var form =  `
+
+        <div class="card login-form">
+          <form id="registrationForm" class="card-body">
+            <h1 class="card-title text-center">Create an Account</h1>
+            <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+
+            <div class="form-group">
+              <label for="first_name">First Name</label>
+              <input type="text" id="first_name" name="first_name" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+              <label for="last_name">Last Name</label>
+              <input type="text" id="last_name" name="last_name" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" id="username" name="username" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" id="email" name="email" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" id="password" name="password" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+              <label for="confirm_password">Confirm Password</label>
+              <input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
+            </div>
+
+            <div>
+              <input type="submit" value="Sign In" class="btn btn-lg btn-primary btn-block">
+            </div>
+          </form>
+        </div>
+    `;
+
+    app.innerHTML=form;
+  }
+
   function postRequest(formId, url){
     let form = document.getElementById(formId);
     form.addEventListener('submit', function(e){
@@ -60,8 +110,8 @@ var authApp = (function() {
 
   return {
     load: function(){
-      loginForm();
-      postRequest('loginForm', '/api/auth/login');
+      registrationForm();
+      postRequest('registrationForm', '/api/auth/register');
     }
   }
 
