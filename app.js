ba9906db766cb5e6d0d6b04eac548918a9121d77
Jason Snider <jason@jasonsnider.com>
2018-12-03 10:56:24 -0600

    Expose session data to the views

--- app.js
+++ app.js
@@ -70,6 +70,11 @@ passport.deserializeUser(function(user, done){
   done(null, user);
 });
 
+app.use(function(req,res,next){
+  res.locals.session = req.session;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
