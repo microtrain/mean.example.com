a415894ea27fc8cb38b94c7c25c235ad5ae28f7e
Jason Snider <jason@jasonsnider.com>
2018-12-04 12:33:45 -0600

    Build a prepopulated web form

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -159,8 +159,55 @@ var usersApp = (function() {
     xhr.send();
 
     xhr.onload = function(){
+      let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
-      console.log(data);
+
+      var form =  `
+        <div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">Edit</h2>
+            <div class="float-right">
+              <a href="#" class="btn btn-primary">Cancel</a>
+            </div>
+          </div>
+          <div class="card-body">
+            <form id="editUser" class="card-body">
+              <input type="hidden" id="_id" name="_id" value="${data.user._id}">
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="first_name">First Name</label>
+                  <input type="text" id="first_name" name="first_name" class="form-control" value="${data.user.first_name}" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="last_name">Last Name</label>
+                  <input type="text" id="last_name" name="last_name" class="form-control" value="${data.user.last_name}" required>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username" class="form-control" value="${data.user.username}" required>
+                </div>
+
+                <div class="form-group col-md-6">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" class="form-control" value="${data.user.email}" required>
+                </div>
+              </div>
+
+              <div class="text-right">
+                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+              </div>
+            </form>
+          </div>
+        </div>
+      `;
+
+      app.innerHTML=form;
     }
   }
 
