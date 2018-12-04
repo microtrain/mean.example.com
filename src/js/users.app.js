14739211ac25e38c3b91f2ffca36564da9fd976d
Jason Snider <jason@jasonsnider.com>
2018-12-04 11:10:27 -0600

    Add a common method for processing web forms

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -72,7 +72,7 @@ var usersApp = (function() {
             </div>
           </div>
           <div class="card-body">
-            <form id="registrationForm" class="card-body">
+            <form id="createUser" class="card-body">
               <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
 
               <div class="row">
@@ -110,6 +110,38 @@ var usersApp = (function() {
     app.innerHTML=form;
   }
 
+  function postRequest(formId, url){
+    let form = document.getElementById(formId);
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+
+      let formData = new FormData(form);
+      let uri = `${window.location.origin}${url}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', uri);
+
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+
+      let object = {};
+      formData.forEach(function(value, key){
+        object[key]=value;
+      });
+
+      xhr.send(JSON.stringify(object));
+      xhr.onload = function(){
+        let data = JSON.parse(xhr.response);
+        if(data.success===true){
+          window.location.hash = `#view-${data.user._id}`
+        }else{
+          document.getElementById('formMsg').style.display='block';
+        }
+      }
+    });
+  }
+
   return {
     load: function(){
       let hash = window.location.hash;
@@ -118,6 +150,7 @@ var usersApp = (function() {
       switch(hashArray[0]){
         case '#create':
           createUser();
+          postRequest('createUser', '/api/users');
           break;
 
         case '#view':
