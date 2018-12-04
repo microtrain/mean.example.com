523576b427ea9ebbbc40c1e8ce941409708e891e
Jason Snider <jason@jasonsnider.com>
2018-12-04 15:44:58 -0600

    Provide a confirmation view

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -205,6 +205,9 @@ var usersApp = (function() {
             </form>
           </div>
         </div>
+        <div>
+          <a href="#delete-${data.user._id}" class="text-danger">Delete</a>
+        </div>
       `;
 
       app.innerHTML=form;
@@ -244,6 +247,51 @@ var usersApp = (function() {
     });
   }
 
+  function deleteView(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let card = '';
+
+      card = `<div class="card bg-transparent border-danger text-danger bg-danger">
+        <div class="card-header bg-transparent border-danger">
+          <h2 class="h3 text-center">Your About to Delete a User</h2>
+        </div>
+        <div class="card-body text-center">
+          <div>
+            Are you sure you want to delete
+            <strong>${data.user.first_name} ${data.user.last_name}</strong>
+          </div>
+
+          <div>Username: <strong>${data.user.username}</strong></div>
+          <div>Email: <strong>${data.user.email}</strong></div>
+
+          <div class="text-center">
+            <br>
+            <a class="btn btn-lg btn-danger text-white">
+              Yes delete ${data.user.username}
+            </a>
+          </div>
+
+        </div>
+      </div>`;
+
+      app.innerHTML = card;
+    }
+  }
+
   return {
     load: function(){
       let hash = window.location.hash;
@@ -264,7 +312,7 @@ var usersApp = (function() {
           break;
 
         case '#delete':
-          console.log('DELETE');
+          deleteView(hashArray[1]);
           break;
 
         default:
